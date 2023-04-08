70ec289becc7a9ed4b2246b91774d6f4839b5303
sabzdotpy <incrediblesabari02@gmail.com>
2023-04-08 20:32:45 +0530

    ready for backend

--- src/views/AppDiseaseId.jsx
+++ src/views/AppDiseaseId.jsx
@@ -1,10 +1,29 @@
-
+import "../styles/AppDiseaseId.scss";
+import ImageRenderer from "../components/ImageRenderer";
+import avatarHmm from "../assets/hmmm.png";
 
 export default function AppDiseaseId() {
 
-    return (
-        <div>
-            Disease Identifier.
-        </div>
-    )
+    const fetchSymptoms = () => {
+        fetch("/api/symptoms").then((res) => res.json().then((data) => {
+            console.log(data)
+        }))
+    }
+        
+    
+
+	return (
+		<div className="appDiseaseId">
+			<section className="topBanner">
+                <button className="fetch" onClick={fetchSymptoms}>Fetch Symptoms</button>
+            </section>
+
+			<footer className="disclaimer">
+				This tool does not provide medical advice It is intended for informational purposes only. It is not a
+				substitute for professional medical advice, diagnosis or treatment. Never ignore professional medical
+				advice in seeking treatment because of something you have read on the WebMD Site. If you think you may
+				have a medical emergency, immediately call your doctor.
+			</footer>
+		</div>
+	);
 }
